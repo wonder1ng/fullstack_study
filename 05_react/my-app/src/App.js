2ce03c0c2be5e31202cb5a7acd685107107e006d
wonder1ng <wonder1ng@velog.io>
2025-02-03 17:06:51 +0900

    react studying

--- 05_react/my-app/src/App.js
+++ 05_react/my-app/src/App.js
@@ -1,5 +1,7 @@
+import "./App.css";
+
 function App() {
-  return <h1 className="greeting">Hello React App!!!</h1>;
+  return <h1 className="App">Hello React App!!!</h1>;
 }
 
 export default App;
---
+++ 05_react/my-app/src/App1.js
@@ -0,0 +1,14 @@
+import E1, { E2, E3, E4 } from "./1-element/Element";
+// default는 이름만, 외는 중괄호 안에 기입
+
+// function App() {
+//   return <h1 className="App">Hello React App~</h1>;
+// }
+function App() {
+  // return E1;
+  // return E2;
+  // return E3;
+  return E4;
+}
+
+export default App;
