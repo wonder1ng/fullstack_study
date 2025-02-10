f933cf406101a33db9636659d78bb19bc3c53b89
wonder1ng <wonder1ng@velog.io>
2025-02-10 09:29:32 +0900

    renamed react dirs and files

--- 05_react/my-app/src/index.js
+++ 05_react/my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // client: FSR server:SSR(next.js)
-import App1 from "./App1";
-import App2 from "./App2";
-import App4 from "./App4";
-import App5 from "./App5";
-import App6 from "./App6";
-import App7 from "./App7";
-import App8 from "./App8";
-import App9 from "./App9";
+import App1 from "./App01";
+import App2 from "./App02";
+import App4 from "./App04";
+import App5 from "./App05";
+import App6 from "./App06";
+import App7 from "./App07";
+import App8 from "./App08";
+import App9 from "./App09";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
