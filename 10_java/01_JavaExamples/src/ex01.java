0f8e3d0241f310be995cd72b04e9f637fef53cb0
wonder1ng <wonder1ng@velog.io>
2025-03-28 15:20:22 +0900

    java studying

---
+++ 10_java/01_JavaExamples/src/ex01.java
@@ -0,0 +1,11 @@
+// 자바는 클래스에서 시작해서 클래스로 끝남.
+// public: 접근제한자
+// class: 클래스 정의어
+public class ex01 {
+    // static: 정적 변수/함수로 선언. 프로그램 시작점에 정의.
+    // String[]: 문자열 배열, 프로그램 시작시 파라미터
+    public static void main(String[] args) {
+        // println: 콘솔 출력
+        System.out.println("Hello Java!");
+    }
+}
---
+++ 10_java/01_first/src/ex01.java
@@ -0,0 +1,30 @@
+public class ex01 {
+    public static void main(String[] args) {
+        System.out.print("Hello print");
+        System.out.println("Hello println");
+        System.out.printf("Hello printf");
+        System.out.print("Hello print\n");
+        System.out.println("Hello println");
+        System.out.printf("%nHello %s%n\n", "printf\n%n");
+
+        int a;
+        int b = 10;
+        // System.out.println(a);
+        // java: variable a might not have been initialized
+        System.out.println(b);
+        a = 2;
+        System.out.println(a);
+        int a1, a2, a3;
+        int b1 = 1, b2 = 1, b3 = 3;
+
+
+        byte c1 = 0;
+        short c2 = 1;
+        int c3 = 2;
+        long c4 = 3L;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
