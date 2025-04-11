3447ea25a3bc0f120faa141e49a97ed1788c98dc
wonder1ng <wonder1ng@velog.io>
2025-04-11 16:30:18 +0900

    spring boot studying

--- 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,23 +6,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question") // URL prefix: 메서드 url의 접두사
 @RequiredArgsConstructor // 상수와 @NotNull 변수 생성자 주입	
 @Controller
 public class QuestionController {
 	
 	private final QuestionRepository questionRepository;
 	
-	@GetMapping("/question/listEx") // Get 방식으로 매핑
+	@GetMapping("/listEx") // Get 방식으로 매핑
 	@ResponseBody // response를 body에 넣음
 	public String listEx() {
 		return "questionList"; // body에 들어갈 response
 	}
 	
-	@GetMapping("/question/list1")
+	@GetMapping("/list1")
 	public String list1(Model model) {
 		List<Question> questionList = this.questionRepository.findAll();
 		model.addAttribute("questionList", questionList);
@@ -32,7 +34,7 @@ public class QuestionController {
 	
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list2")
+	@GetMapping("/list2")
 	public String list2(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
@@ -40,7 +42,7 @@ public class QuestionController {
 	}
 	
 	// 중괄호 안은 값이 들어갈 변수명
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	// @PathVariable이 url과 연결해줌
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
