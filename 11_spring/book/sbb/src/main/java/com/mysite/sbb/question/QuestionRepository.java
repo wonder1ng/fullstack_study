ff7b912e15540a5c281aeca467ea3e7e3db9e68c
wonder1ng <wonder1ng@velog.io>
2025-04-23 18:03:46 +0900

    spring boot studying

--- 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	// JPA는 리포지터리의 메서드명을 분석하여 쿼리를 만들고 실행함.
@@ -14,4 +17,23 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	Page<Question> findAll(Pageable pageable);
 	// Page: 페이징 클래스
 	// Pageable: 페이징 처리 인터페이스
+	Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+	
+	// @Query: 직접 쿼리 입력(Entity 기준으로 테이블명 등 작성)
+	@Query("select "
+			+ "distinct q "
+			+ "from Question q "
+			+ "left outer join SiteUser u1 on q.author=u1 "
+			+ "left outer join Answer a on a.question=1 "
+			+ "left outer join SiteUser u2 on a.author=u2 "
+			+ "where "
+			+ " q.subject like %:kw% "
+			+ " or q.content like %:kw% "
+			+ " or u1.username like %:kw% "
+			+ " or a.content like %:kw% "
+			+ " or a.content like %:kw% "
+			+ " or u2.username like %:kw%"
+			+ "")
+	// @Param: @Query에 들어갈 값 입력 `:kw` = `kw`
+	Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
--- 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ 11_spring/book/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -9,11 +9,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -22,12 +30,33 @@ public class QuestionService {
 	
 	private final QuestionRepository questionRepository;
 	
-	public Page<Question> getList(int page) {
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true); // 중복 제거
+				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+				Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+						cb.like(q.get("content"), "%" + kw + "%"), // 내용
+						cb.like(u1.get("username"), "%" + kw + "%"), // 질문 작성자
+						cb.like(a.get("content"), "%" + kw + "%"), // 답변 내용
+						cb.like(u2.get("username"), "%" + kw + "%")); // 답변 작성자
+			}
+		};
+	}
+	
+	public Page<Question> getList(int page, String kw) {
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		// PageRequest: 페이징 요청 클래스
-		return this.questionRepository.findAll(pageable);
+		Specification<Question> spec = search(kw);
+		return this.questionRepository.findAll(spec, pageable);
+		// @Query로 작성된 findAllByKeyword 사용하는 방법
+		// return this.questionRepository.findAllByKeyword(kw, pageable);
 	}
 	
 	public Question getQuestion(Integer id) {
@@ -46,5 +75,21 @@ public class QuestionService {
 		q.setCreateDate(LocalDateTime.now());
 		q.setAuthor(user);
 		this.questionRepository.save(q);
+	}
+	
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+	}
+	
+	public void delete(Question question) {
+		this.questionRepository.delete(question);
+	}
+	
+	public void vote(Question question, SiteUser siteUser) {
+		question.getVoter().add(siteUser);
+		this.questionRepository.save(question);
 	}
 }
